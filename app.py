831570f0b0c79e69d0c7a1399a9b127925a13bb9
Fabián Vargas <fabidev18@gmail.com>
2024-03-07 20:03:13 -0600

    main documentation of functions (DocString)

--- app.py
+++ app.py
@@ -2,33 +2,54 @@ import ARMem as ar
 import random
 import time
 
-# (level, difficulty)
+# (level, marks)
 levels = [(1, 3), (2, 4), (3, 5)]
+
+# Recording of each player's data, like a NoSQL database.
+# Each player contains the data of his username, time per level and total time.
 players: dict = {}
 
-def request_data():
+# Game functions
+def request_data() -> dict:
+  """Prompt the user for all necessary information to customize the game.
+
+    Returns:
+        dict: All information obtained from the user.
+  """
   amount_of_players = int(input("Cantidad de jugadores: "))
 
   while amount_of_players <= 0:
     print("La cantidad de jugadores debe ser un número entero mayor a 0")
     amount_of_players = int(input("Cantidad de jugadores: "))
 
-  return amount_of_players
+  return {amount_of_players}
 
 def init_players(amount: int = 1):
-  i = 0
-  while len(players.keys()) < amount:
-    new_player = (input(f"Nombre del jugador {i + 1}: "))
+  """Initialize player data structure.
+
+    Args:
+        amount (int, optional): Number of players. Defaults to 1.
+  """
+  
+  for n in range(1, amount):
+    new_player = (input(f"Nombre del jugador {n}: "))
     players[new_player] = {
       "Nivel 1": [],
       "Nivel 2": [],
       "Nivel 3": [],
       "Tiempo Total": 0
     }
-    i += 1
-  print(players)
 
 def generate_game(amount: int):
+  """Generate a random game sequence.
+
+    Args:
+        amount (int): Length of the game sequence.
+
+    Returns:
+        list: Randomly generated game sequence.
+  """
+
   game = []
 
   while len(game) < amount:
@@ -40,6 +61,12 @@ def generate_game(amount: int):
   return game
 
 def round(lvl: int):
+  """Execute a round of the game for each player.
+
+    Args:
+        level (int): Current level of the game.
+  """
+
   for player in players.keys():
     game = generate_game(lvl[1])
     print(f"{player} memoriza lo siguiente: {game}")
@@ -48,10 +75,19 @@ def round(lvl: int):
     players[player][lvl].append(round_time)
 
 def play_level(level: int):
+  """Play a specific level of the game.
+
+    Args:
+        level (int): Level of the game to play.
+  """
+
   for x in range(4):
     round(level)
   
 def main():
+  """
+    Main function to run the game.
+  """
   amount_of_players = request_data()
   init_players(amount_of_players)
   
