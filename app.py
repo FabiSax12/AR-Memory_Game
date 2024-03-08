d8da64042af126de21039520c61a172d0031efb5
Fabián Vargas <fabidev18@gmail.com>
2024-03-07 19:03:01 -0600

    Function with de logic of Round

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import ARMem as ar
 import random
+import time
 
+# (level, difficulty)
+levels = [(1, 3), (2, 4), (3, 5)]
 players: dict = {}
 
 def request_data():
-  quantity_of_players = int(input("Cantidad de jugadores: "))
+  amount_of_players = int(input("Cantidad de jugadores: "))
 
-  while quantity_of_players <= 0:
+  while amount_of_players <= 0:
     print("La cantidad de jugadores debe ser un número entero mayor a 0")
-    quantity_of_players = int(input("Cantidad de jugadores: "))
+    amount_of_players = int(input("Cantidad de jugadores: "))
 
-  return quantity_of_players
+  return amount_of_players
 
-def init_players(quantity: int = 1):
+def init_players(amount: int = 1):
   i = 0
-  while len(players.keys()) < quantity:
+  while len(players.keys()) < amount:
     new_player = (input(f"Nombre del jugador {i + 1}: "))
     players[new_player] = {
       "Nivel 1": [],
@@ -25,24 +28,31 @@ def init_players(quantity: int = 1):
     i += 1
   print(players)
 
-def generate_game(quantity: int):
+def generate_game(amount: int):
   game = []
 
-  while len(game) < quantity:
-    random_number = random.randint(0, quantity - 1)
+  while len(game) < amount:
+    random_number = random.randint(0, amount - 1)
     
     if random_number not in game:
       game.append(random_number)
 
   return game
 
+def round(lvl: int):
+  for player in players.keys():
+    game = generate_game(levels[lvl - 1][1])
+    print(f"{player} memoriza lo siguiente: {game}")
+    time.sleep(2)
+    round_time = round(ar.start_sorting(game,flip_image=False, show_images=True), 2)
+    players[player][lvl].append(round_time)
+
 def main():
-  quantity_of_players = request_data()
-  init_players(quantity_of_players)
-  game = generate_game(5)
+  amount_of_players = request_data()
+  init_players(amount_of_players)
+  
+  round(1)
 
-  time = ar.start_sorting(game)
-
-  print(time)
+  print(players)
 
 main()
