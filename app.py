1298f558c7a37abd58db25741781d270cf585b7d
Fabián Vargas <fabidev18@gmail.com>
2024-03-13 15:25:48 -0600

    Function of generating a report of times by level

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def init_players(amount: int = 1):
   for n in range(amount):
     new_player = (input(f"Nombre del jugador {n+1}: "))
     players[new_player] = {
-      "Nivel 1": [],
-      "Nivel 2": [],
-      "Nivel 3": [],
+      "Nivel 1": 0,
+      "Nivel 2": 0,
+      "Nivel 3": 0,
       "Tiempo Total": 0
     }
 
@@ -53,14 +53,14 @@ def generate_game(amount: int):
   game = []
 
   while len(game) < amount:
-    random_number = random.randint(0, amount - 1)
+    random_number = random.randint(0, 4)
     
     if random_number not in game:
       game.append(random_number)
 
   return game
 
-def play_round(lvl: int):
+def play_round(lvl: int): 
   """Execute a round of the game for each player.
 
     Args:
@@ -68,12 +68,19 @@ def play_round(lvl: int):
   """
 
   for player in players.keys():
-    # game = generate_game(lvl[1])
-    game = [2]
+    game = generate_game(lvl[1])
     print(f"{player} memoriza lo siguiente: {game}")
-    time.sleep(2)
-    round_time = round(ar.start_sorting(game,flip_image=False, show_images=True), 2)
-    players[player][f"Nivel {lvl}"].append(round_time)
+    # time.sleep(2)
+    # round_time = round(ar.start_sorting(game,flip_image=False, show_images=True), 2)
+    round_time = round(random.random() * 10)
+    # time.sleep(round_time)
+    players[player][f"Nivel {lvl[0]}"] += round_time
+
+def level_report(lvl):
+  print(f"Reporte del nivel {lvl}")
+  for player in players.keys():
+    report = f"{player} {players[player][f"Nivel {lvl}"]} seg"
+    print(report)
 
 def play_level(level: int):
   """Play a specific level of the game.
@@ -81,10 +88,14 @@ def play_level(level: int):
     Args:
         level (int): Level of the game to play.
   """
+  print(f"Nivel {level[0]}")
 
   for x in range(5):
+    print(f"Ronda {x + 1}")
     play_round(level)
   
+  level_report(level[0])
+
 def main():
   """
     Main function to run the game.
@@ -93,8 +104,6 @@ def main():
   init_players(amount_of_players)
   
   for lvl in levels:
-    play_level(lvl[0])
-
-  print(players)
+    play_level(lvl)
 
 main()
